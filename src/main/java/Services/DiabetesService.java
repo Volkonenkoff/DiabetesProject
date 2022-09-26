37e2c64beb797b25623f4db4ef48cd8d43222613
Никита Лукашов <nikita.lukashov2001@mail.ru>
2022-09-26 21:28:24 +0300

    Commit

--- src/main/java/Services/DiabetesService.java
+++ src/main/java/Services/DiabetesService.java
@@ -6,10 +6,9 @@ import org.springframework.stereotype.Service;
 @Service
 public class DiabetesService {
 
-    public Diabetes createDiabetes(int patientId, int age, String currentDiabetesType, String initialDiabetesType,
+    public void createDiabetes(int patientId, int age, String currentDiabetesType, String initialDiabetesType,
                                    char gender, boolean heredity, boolean insulinTherapy, boolean disability,
                                    String complications, String yearOfDiagnosis, double weight, double height, double BMI)
     {
-        return D
     }
 }
